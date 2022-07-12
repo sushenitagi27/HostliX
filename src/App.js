396679a523598089fd15e02f8b7af078a36b5f72
Sushen Itagi <sushenitagi27@gmail.com>
2022-07-12 21:14:26 +0530

    HosteliX

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import './App.scss';
 import Layout from './components/Layout';
 import { Routes,Route } from 'react-router-dom'
 import Home from './components/Home';
+import Contact from './components/Contact';
+import About from './components/About';
 function App() {
   return (
     <>
     <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
+          <Route path="/about" element={<About/>}/>
+          <Route path="/contact" element={<Contact/>}/>
           </Route>
     </Routes>
     </>
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,22 +1,44 @@
 import { Link } from 'react-router-dom';
 import LogoTitle from '../../assets/images/logohome.png';
+import AnimatedLetters from '../AnimatedLetters';
 import './index.scss';
+import {  useState } from 'react'
+import Logo from './Logo';
+import Loader from 'react-loaders';
 const Home = () => 
 {
+    const [letterClass] = useState('text-animate')
+
+    const nameArray = ['o','s','t','e','l','i','X']
+    const jobArray = ['p','r','i','v','a','t','e',' ','l','i','m','i','t','e','d']
+  
+    // useEffect(() => {
+    //   return setTimeout(() => {
+    //     setLetterClass('text-animate-hover')
+    //   }, 4000)
+    // }, [])
     return(
+        <>
         <div className="container home-page">
             <div className="text-zone">
                 <h1>
                 <img src={LogoTitle} alt="developer"/>
-                osteliX
+                <AnimatedLetters letterClass={letterClass} strArray={nameArray} idx={15}/>
                 <br/>
-                private limited
+                
+                <AnimatedLetters
+                 letterClass={letterClass}
+                 strArray={jobArray}
+                 idx={21}
+                 />
                 </h1>
                 <h2>| Economic Consultation | Wealth Expert |  </h2>
                 <Link to='/contact' className='flat-button'>Reach Us</Link>
             </div>
-
+            <Logo/>
         </div>
+        <Loader type='line-scale'></Loader>
+        </>
     )
 
 
